cf105601366ca8439e592723a65865c6d351aca9
Faith Ugbeshe <faithnneba@gmail.com>
2021-01-13 21:43:55 +0100

    added post app using laravel

---
+++ resources/views/emails/posts/post_liked.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# Your post was liked
+
+{{ $liker->name }} liked one of your posts
+
+The body of your message.
+
+@component('mail::button', ['url' => route('posts.show', $post)])
+    View post
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent 
+  
